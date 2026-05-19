e8adb88ff2060552918172a8ca273429005b068b
zero314 <zero3.14159265358979323846@gmail.com>
2026-05-19 14:41:51 +0800

    新增圖書管理程式 UI 與功能
    新增多個 UI 元件，包括分割容器、書籍清單、借書清單等，並調整視窗大小與標題。
    實作書籍清單初始化邏輯，包含大圖示與小圖示的生成、詳細資料欄位設定，以及檢視模式切換功能。
    新增雙擊書籍加入借書清單的功能。
    移除 `Resources1.Designer.cs` 檔案及相關編譯設定。

--- 1121538_徐霈綺_圖書管理程式/Form1.Designer.cs
+++ 1121538_徐霈綺_圖書管理程式/Form1.Designer.cs
@@ -29,12 +29,122 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.listViewBooks = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxView = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBoxBorrowed = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.listViewBooks);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.listBoxBorrowed);
+            this.splitContainer1.Panel2.Controls.Add(this.label2);
+            this.splitContainer1.Panel2.Controls.Add(this.comboBoxView);
+            this.splitContainer1.Panel2.Controls.Add(this.label1);
+            this.splitContainer1.Panel2.Padding = new System.Windows.Forms.Padding(10);
+            this.splitContainer1.Size = new System.Drawing.Size(850, 550);
+            this.splitContainer1.SplitterDistance = 600;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // listViewBooks
+            // 
+            this.listViewBooks.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listViewBooks.HideSelection = false;
+            this.listViewBooks.Location = new System.Drawing.Point(0, 0);
+            this.listViewBooks.Name = "listViewBooks";
+            this.listViewBooks.Size = new System.Drawing.Size(600, 550);
+            this.listViewBooks.TabIndex = 0;
+            this.listViewBooks.UseCompatibleStateImageBehavior = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(13, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "檢視方式：";
+            // 
+            // comboBoxView
+            // 
+            this.comboBoxView.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxView.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.comboBoxView.FormattingEnabled = true;
+            this.comboBoxView.Items.AddRange(new object[] {
+            "大圖示",
+            "詳細資料",
+            "小圖示",
+            "清單",
+            "大圖示加詳細資料"});
+            this.comboBoxView.Location = new System.Drawing.Point(17, 43);
+            this.comboBoxView.Name = "comboBoxView";
+            this.comboBoxView.Size = new System.Drawing.Size(210, 28);
+            this.comboBoxView.TabIndex = 1;
+            this.comboBoxView.SelectedIndexChanged += new System.EventHandler(this.comboBoxView_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(13, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "借書清單：";
+            // 
+            // listBoxBorrowed
+            // 
+            this.listBoxBorrowed.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.listBoxBorrowed.FormattingEnabled = true;
+            this.listBoxBorrowed.ItemHeight = 20;
+            this.listBoxBorrowed.Location = new System.Drawing.Point(17, 118);
+            this.listBoxBorrowed.Name = "listBoxBorrowed";
+            this.listBoxBorrowed.Size = new System.Drawing.Size(210, 404);
+            this.listBoxBorrowed.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(850, 550);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "圖書管理程式";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListView listViewBooks;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBoxView;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBoxBorrowed;
     }
 }
 
--- 1121538_徐霈綺_圖書管理程式/Form1.cs
+++ 1121538_徐霈綺_圖書管理程式/Form1.cs
@@ -16,5 +16,98 @@ namespace _1121538_徐霈綺_圖書管理程式
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] books = { "三國演義", "西遊記", "唐詩三百首", "楚辭", "西廂記", "水滸傳", "紅樓夢", "牡丹亭" };
+            
+            ImageList imgLarge = new ImageList();
+            imgLarge.ImageSize = new Size(100, 140);
+            imgLarge.ColorDepth = ColorDepth.Depth32Bit;
+
+            ImageList imgSmall = new ImageList();
+            imgSmall.ImageSize = new Size(32, 32);
+            imgSmall.ColorDepth = ColorDepth.Depth32Bit;
+
+            for (int i = 0; i < books.Length; i++)
+            {
+                // 產生空白(灰色)的大圖
+                Bitmap bmpLarge = new Bitmap(100, 140);
+                using (Graphics g = Graphics.FromImage(bmpLarge))
+                {
+                    g.Clear(Color.Gray);
+                    // 畫黃色垂直文字作為書籍封面示意
+                    StringFormat format = new StringFormat();
+                    format.FormatFlags = StringFormatFlags.DirectionVertical;
+                    using (Font f = new Font("微軟正黑體", 12))
+                    using (Brush b = new SolidBrush(Color.Yellow))
+                    {
+                        g.DrawString(books[i], f, b, new RectangleF(70, 10, 30, 120), format);
+                    }
+                }
+                imgLarge.Images.Add(bmpLarge);
+
+                // 產生空白(灰色)的小圖
+                Bitmap bmpSmall = new Bitmap(32, 32);
+                using (Graphics g = Graphics.FromImage(bmpSmall))
+                {
+                    g.Clear(Color.Gray);
+                }
+                imgSmall.Images.Add(bmpSmall);
+            }
+
+            listViewBooks.LargeImageList = imgLarge;
+            listViewBooks.SmallImageList = imgSmall;
+
+            // 增加詳細資料的欄位
+            listViewBooks.Columns.Add("書名", 150);
+            listViewBooks.Columns.Add("作者", 150);
+            listViewBooks.Columns.Add("出版年份", 100);
+
+            for (int i = 0; i < books.Length; i++)
+            {
+                ListViewItem item = new ListViewItem(books[i], i);
+                item.SubItems.Add("作者佚名");
+                item.SubItems.Add("西元前~清代");
+                listViewBooks.Items.Add(item);
+            }
+
+            // 預設為大圖示
+            comboBoxView.SelectedIndex = 0;
+
+            // 借書事件 (雙擊加入清單)
+            listViewBooks.DoubleClick += ListViewBooks_DoubleClick;
+        }
+
+        private void ListViewBooks_DoubleClick(object sender, EventArgs e)
+        {
+            if (listViewBooks.SelectedItems.Count > 0)
+            {
+                string bookName = listViewBooks.SelectedItems[0].Text;
+                listBoxBorrowed.Items.Add(bookName);
+            }
+        }
+
+        private void comboBoxView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBoxView.SelectedIndex)
+            {
+                case 0:
+                    listViewBooks.View = View.LargeIcon;
+                    break;
+                case 1:
+                    listViewBooks.View = View.Details;
+                    break;
+                case 2:
+                    listViewBooks.View = View.SmallIcon;
+                    break;
+                case 3:
+                    listViewBooks.View = View.List;
+                    break;
+                case 4:
+                    listViewBooks.View = View.Tile;
+                    break;
+            }
+        }
     }
 }
