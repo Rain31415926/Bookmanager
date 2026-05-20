a1d53c24add79a4bb088696fd4c479e3bbc1fbea
zero314 <zero3.14159265358979323846@gmail.com>
2026-05-20 20:52:46 +0800

    新增功能與修復程式錯誤
    此提交包含以下更改：
    -  導入圖片和新增圖書

--- 1121538_徐霈綺_圖書管理程式/Form1.cs
+++ 1121538_徐霈綺_圖書管理程式/Form1.cs
@@ -19,8 +19,8 @@ namespace _1121538_徐霈綺_圖書管理程式
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            string[] books = { "三國演義", "西遊記", "唐詩三百首", "楚辭", "西廂記", "水滸傳", "紅樓夢", "牡丹亭" };
-            
+            string[] books = { "三國演義", "西遊記", "唐詩三百首", "楚辭", "西廂記", "水滸傳", "紅樓夢", "牡丹亭", "地獄變" };
+
             ImageList imgLarge = new ImageList();
             imgLarge.ImageSize = new Size(100, 140);
             imgLarge.ColorDepth = ColorDepth.Depth32Bit;
@@ -29,31 +29,53 @@ namespace _1121538_徐霈綺_圖書管理程式
             imgSmall.ImageSize = new Size(32, 32);
             imgSmall.ColorDepth = ColorDepth.Depth32Bit;
 
+            // 取得對應 Resources 的 ResourceManager
+            System.Resources.ResourceManager rm = Properties.Resources.ResourceManager;
+
             for (int i = 0; i < books.Length; i++)
             {
-                // 產生空白(灰色)的大圖
-                Bitmap bmpLarge = new Bitmap(100, 140);
-                using (Graphics g = Graphics.FromImage(bmpLarge))
-                {
-                    g.Clear(Color.Gray);
-                    // 畫黃色垂直文字作為書籍封面示意
-                    StringFormat format = new StringFormat();
-                    format.FormatFlags = StringFormatFlags.DirectionVertical;
-                    using (Font f = new Font("微軟正黑體", 12))
-                    using (Brush b = new SolidBrush(Color.Yellow))
-                    {
-                        g.DrawString(books[i], f, b, new RectangleF(70, 10, 30, 120), format);
-                    }
-                }
-                imgLarge.Images.Add(bmpLarge);
+                Image resImage = null;
 
-                // 產生空白(灰色)的小圖
-                Bitmap bmpSmall = new Bitmap(32, 32);
-                using (Graphics g = Graphics.FromImage(bmpSmall))
+                // 嘗試從 Resources 取得對應的圖片 (找 Book1, Book2... 或是 book9 等小寫)
+                object obj = rm.GetObject("Book" + (i + 1));
+                if (obj == null)
                 {
-                    g.Clear(Color.Gray);
+                    // 嘗試小寫開頭的，因為您有一張叫做 book9
+                    obj = rm.GetObject("book" + (i + 1));
                 }
-                imgSmall.Images.Add(bmpSmall);
+                resImage = (Image)obj;
+
+                if (resImage != null)
+                {
+                    imgLarge.Images.Add(resImage);
+                    imgSmall.Images.Add(resImage);
+                }
+                else
+                {
+                    // 產生空白(灰色)的大圖
+                    Bitmap bmpLarge = new Bitmap(100, 140);
+                    using (Graphics g = Graphics.FromImage(bmpLarge))
+                    {
+                        g.Clear(Color.Gray);
+                        // 畫黃色垂直文字作為書籍封面示意
+                        StringFormat format = new StringFormat();
+                        format.FormatFlags = StringFormatFlags.DirectionVertical;
+                        using (Font f = new Font("微軟正黑體", 12))
+                        using (Brush b = new SolidBrush(Color.Yellow))
+                        {
+                            g.DrawString(books[i], f, b, new RectangleF(70, 10, 30, 120), format);
+                        }
+                    }
+                    imgLarge.Images.Add(bmpLarge);
+
+                    // 產生空白(灰色)的小圖
+                    Bitmap bmpSmall = new Bitmap(32, 32);
+                    using (Graphics g = Graphics.FromImage(bmpSmall))
+                    {
+                        g.Clear(Color.Gray);
+                    }
+                    imgSmall.Images.Add(bmpSmall);
+                }
             }
 
             listViewBooks.LargeImageList = imgLarge;
@@ -64,11 +86,14 @@ namespace _1121538_徐霈綺_圖書管理程式
             listViewBooks.Columns.Add("作者", 150);
             listViewBooks.Columns.Add("出版年份", 100);
 
+            string[] authors = { "羅貫中", "吳承恩", "孫洙(蘅塘退士)", "屈原 等", "王實甫", "施耐庵", "曹雪芹", "湯顯祖", "芥川龍之介" };
+            string[] years = { "明代", "明代", "清代", "戰國時代", "元代", "明代", "清代", "明代", "大正7年" };
+
             for (int i = 0; i < books.Length; i++)
             {
                 ListViewItem item = new ListViewItem(books[i], i);
-                item.SubItems.Add("作者佚名");
-                item.SubItems.Add("西元前~清代");
+                item.SubItems.Add(authors[i]);
+                item.SubItems.Add(years[i]);
                 listViewBooks.Items.Add(item);
             }
 
--- 1121538_徐霈綺_圖書管理程式/Properties/Resources1.Designer.cs
+++ 1121538_徐霈綺_圖書管理程式/Properties/Resources1.Designer.cs
@@ -109,5 +109,45 @@ namespace _1121538_徐霈綺_圖書管理程式.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   查詢類型 System.Drawing.Bitmap 的當地語系化資源。
+        /// </summary>
+        internal static System.Drawing.Bitmap Book6 {
+            get {
+                object obj = ResourceManager.GetObject("Book6", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   查詢類型 System.Drawing.Bitmap 的當地語系化資源。
+        /// </summary>
+        internal static System.Drawing.Bitmap Book7 {
+            get {
+                object obj = ResourceManager.GetObject("Book7", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   查詢類型 System.Drawing.Bitmap 的當地語系化資源。
+        /// </summary>
+        internal static System.Drawing.Bitmap Book8 {
+            get {
+                object obj = ResourceManager.GetObject("Book8", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   查詢類型 System.Drawing.Bitmap 的當地語系化資源。
+        /// </summary>
+        internal static System.Drawing.Bitmap book9 {
+            get {
+                object obj = ResourceManager.GetObject("book9", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
